3308612c3319f9b79b4643fc12923871fb025383
Regina <reginaguagnelli@gmail.com>
2022-07-06 18:07:39 -0500

    intentando agregar el mapa y algunos esilos

--- screens/IssLocationScreen.js
+++ screens/IssLocationScreen.js
@@ -7,32 +7,102 @@ import {
   StatusBar,
   SafeAreaView, //otra pista
   Platform,
+  Alert,
+  Image
 } from "react-native";
 
+import axios from "axios"
+
+
+
+import MapView, {Marker} from "react-native-maps";
+
 export default class IssLocationScreen extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: {},
+    }
+  }
+
+  // componentDidMount
+componentDidMount() {
+  this.getIssLocation();
+}
+
+// funcion que hay que hacer
+getIssLocation = () =>{
+  axios.get("https://api.wheretheiss.at/v1/satellites/25544.").then((response)=>{
+    this.setState({location: response.data}).catch((error)=>{
+      Alert.alert(error.message)
+    })
+  })
+}
+
   render() {
-    return (
-      <View style={styles.container}>
-        {/* ##########PRIMERA PARTE DEL DESAFIO ########## */}
-        {/* agrega el componente SafeAreaView */}
 
-        {/* Agrega el componente para la imagen de fondo */}
+    if (Object.keys(this.state.location).length ===0) {
+      <View
+      style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
 
-        {/* este texto ponlo dentro de un componente View y no olvides el estilo :D */}
-        <Text>¡Pantalla de ubicación EEI!</Text>
+        <Text>Cargando......</Text>
+        </View>
+      
+    } else {
 
-        {/* ######################################## */}
+      return (
+        <View style={styles.container}>
+          {/* ##########PRIMERA PARTE DEL DESAFIO ########## */}
+          <SafeAreaView styles={styles.droidSafeArea}/>
+          <ImageBackground>
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>¡Localización de la EEI!</Text>
+            </View>
+            <View styles={styles.mapContainer }>
+              <MapView style={styles.map}
+              
+              region={{
+                latitude: this.state.location.latitude,
+                longitude: this.state.location.longitude,
+                latitudeDelta: 100,
+                latitudeDelta: 100,
+              }}
+              >
+              <Marker
+                coordinate={{
+                  latitude: this.state.location.latitude,
+                  longitude: this.state.location.longitude,
+                }}
+              >
+                <Image
+                  source={require("../assets/iss_bg.jpg")}
+                  style={{height:50, width: 50}}
+                />
+              </Marker>
+              </MapView>
+  
+            </View>
+          </ImageBackground>
+  
+  
+  
+          {/* ##########SEGUNDA PARTE DEL DESAFIO ########## */}
+  
+          {/* agrega un view, dentro de este un componente Marker, dentro de este, un componente Image */}
+          {/* ######################################## */}
+  
+          {/* ##########TERCERA Y ULTIMA PARTE DEL DESAFIO ########## */}
+  
+          {/* debes extraer la informacion de la api, no olvide encerrar todo en un View */}
+        </View>
+      );
 
-        {/* ##########SEGUNDA PARTE DEL DESAFIO ########## */}
+      
+    }
 
-        {/* agrega un view, dentro de este un componente Marker, dentro de este, un componente Image */}
-        {/* ######################################## */}
 
-        {/* ##########TERCERA Y ULTIMA PARTE DEL DESAFIO ########## */}
-
-        {/* debes extraer la informacion de la api, no olvide encerrar todo en un View */}
-      </View>
-    );
   }
 }
 
